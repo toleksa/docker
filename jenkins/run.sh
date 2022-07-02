42a6f0b1dba3d94044245eea59e0abf1446a8157
toleksa <toleksa@gmail.com>
2022-07-02 10:22:51 +0200

    init

---
+++ jenkins/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker stop jenkins
+docker rm jenkins
+
+docker run -d -p 9003:8080 --name jenkins --restart=on-failure --mount type=bind,source=/data/jenkins/,target=/var/jenkins_home jenkins/jenkins:lts-jdk11
+
