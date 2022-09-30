2604ef95d26fc31edfda9ab2953cd674165325c1
toleksa <toleksa@gmail.com>
2022-09-30 21:04:11 +0200

    init

---
+++ cofimon2/mon/cofi/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+#from .models import Babe, SiteBabe, AllBabe, Vote, ExternalSite
+
+#admin.site.register(Babe)
+#admin.site.register(SiteBabe)
+#admin.site.register(AllBabe)
+#admin.site.register(Vote)
+#admin.site.register(ExternalSite)
+
