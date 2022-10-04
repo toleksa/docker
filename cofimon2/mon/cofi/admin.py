3e2affead8c2857d67f8b5fbda1ce154e10b4393
toleksa <toleksa@gmail.com>
2022-10-04 22:32:07 +0200

    models

--- cofimon2/mon/cofi/admin.py
+++ cofimon2/mon/cofi/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-#from .models import Babe, SiteBabe, AllBabe, Vote, ExternalSite
+from .models import Report
+from .models import ValidInstance
 
-#admin.site.register(Babe)
-#admin.site.register(SiteBabe)
-#admin.site.register(AllBabe)
-#admin.site.register(Vote)
-#admin.site.register(ExternalSite)
-
+admin.site.register(Report)
+admin.site.register(ValidInstance)
