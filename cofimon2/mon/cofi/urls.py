fc6eb990b8000bd6d828540d39aa5bb99a7a293f
toleksa <toleksa@gmail.com>
2022-10-05 23:13:49 +0200

    urls

--- cofimon2/mon/cofi/urls.py
+++ cofimon2/mon/cofi/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage),
+  path('', views.mainpage, name='mainpage'),
+  path('instance/<str:instance_id>', views.instance, name='instance'),
+  path('form/', views.report_upload, name='report_upload'),
+  path('api/', views.api, name='api'),
 ]
