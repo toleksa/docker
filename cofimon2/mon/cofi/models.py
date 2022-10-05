9d70c7c9e64a9ac9c57a8a15c63ea13541c82a90
toleksa <toleksa@gmail.com>
2022-10-05 22:55:00 +0200

    fields update

--- cofimon2/mon/cofi/models.py
+++ cofimon2/mon/cofi/models.py
@@ -9,12 +9,13 @@ def get_upload_path(instance, filename):
   return os.path.join("%s" % instance.instance, filename)
 
 class Report(models.Model):
-  instance = models.CharField(max_length=20)
-  status = models.IntegerField()
-  cofistatus = models.IntegerField(blank=True,null=True)
+  id = models.IntegerField(primary_key=True, unique=True)
+  instance = models.CharField(max_length=20, default=None, blank=False, null=False)
+  status = models.IntegerField(default=None, blank=False, null=False)
+  cofistatus = models.IntegerField(default=None, blank=False, null=False)
   date = models.DateTimeField(auto_now_add=True)
-  document = models.FileField(upload_to=get_upload_path)
-  cofidocument = models.FileField(upload_to=get_upload_path,blank=True)
+  impfile = models.FileField(upload_to=get_upload_path, blank=False, null=False)
+  dlfile = models.FileField(upload_to=get_upload_path, blank=False, null=False)
 
 class ValidInstance(models.Model):
   name = models.CharField(max_length=20)
