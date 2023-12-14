d03cf2fda69b32abc51f5336660fbbb4767c531d
toleksa <toleksa@gmail.com>
2023-12-14 22:16:35 +0200

    jenkins - move data to /local

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -3,5 +3,5 @@
 docker stop jenkins
 docker rm jenkins
 
-docker run -d -p 9003:8080 --name jenkins --restart=on-failure --mount type=bind,source=/data/jenkins/,target=/var/jenkins_home jenkins/jenkins:lts-jdk11
+docker run -d -p 9003:8080 --name jenkins --restart=on-failure --mount type=bind,source=/local/jenkins/,target=/var/jenkins_home jenkins/jenkins:lts-jdk11
 
